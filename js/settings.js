1afeb49b16695a941929ec9adfa51b27f3dec960
yangyaxin420 <251212752@qq.com>
2026-07-09 11:37:42 +0800

    chore: v3.4.0 晞晞日报功能上线

--- js/settings.js
+++ js/settings.js
@@ -1,5 +1,5 @@
 /* ==================== 设置页 ==================== */
-const APP_VERSION = "v3.3.0 (2026.07.07)";
+const APP_VERSION = "v3.4.0 (2026.07.09)";
 
 const DEFAULT_SETTINGS = {
   proactiveMsg: true,
